192d08df9efb6da8d307ac75d871c280334c40ec
Gagik <114832503+Gago2077@users.noreply.github.com>
2025-03-23 19:53:55 +0400

    Added models, advanced hp calculating system

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,11 +6,12 @@ public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance { get; private set; }
 
-    private uint _currentLevel = 1;
-    private int _levelMaxDifficulty;
-    private int _levelMinDifficulty;
+    private int _currentLevel = 100;
+    private float _levelMaxDifficulty;
+    private float _levelMinDifficulty;
     private int _currentScore=0;
     private int _desiredScore=0;
+    [SerializeField] private float _difficultyMultiplier = 1.1f;
     private void Awake()
     {
         if (Instance == null)
@@ -26,9 +27,9 @@ public class LevelManager : MonoBehaviour
 
     public int GetHealth()
     {
-        _levelMinDifficulty = (int)_currentLevel * 10;
-        _levelMaxDifficulty = (int)_currentLevel * 20;
-        return Random.Range(_levelMinDifficulty, _levelMaxDifficulty);
+        _levelMinDifficulty = _currentLevel  * _difficultyMultiplier;
+        _levelMaxDifficulty = _currentLevel * 2 * _difficultyMultiplier;
+        return Random.Range((int)_levelMinDifficulty, (int)_levelMaxDifficulty);
     }
 
 }
